9b11bf7093201f31b91288ff83f2b57035b42899
Mahir-Neema <mohi10503@gmail.com>
2023-05-15 14:57:59 +0530

    added auth router

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 const mongoUrl = process.env.MONGO_URL
 
@@ -13,9 +15,7 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
 
-app.use("/",(req,res)=>{
-    console.log("fndsf");
-})
+app.use("/api/auth", authRoute);
 
 app.listen("5000",()=>{
     console.log("Backend running");
