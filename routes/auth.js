98fadc4088f5ddc21870bf6cebc881592b4f3418
Mahir-Neema <mohi10503@gmail.com>
2023-05-15 15:42:53 +0530

    login wrong cred bug fixed

--- routes/auth.js
+++ routes/auth.js
@@ -26,11 +26,14 @@ router.post("/register", async(req,res)=>{
 router.post("/login", async (req,res)=>{
     try{
         const user = await User.findOne({username: req.body.username});
-        !user && res.status(400).json("wrong credentials");
+        if(!user){
+            return res.status(400).json("wrong credentials");
+        }
 
         const validated = await bcrypt.compare(req.body.password, user.password);
-        !validated && res.status(400).json("wrong credentials");
-
+        if(!validated){
+            return res.status(400).json("wrong credentials");
+        }
         res.status(200).json(user);
     } catch(e){
         res.status(500).json(e);
