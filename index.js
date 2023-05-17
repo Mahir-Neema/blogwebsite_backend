2f9a63aeedf83f01e21997cb5897e3e24ccb9ab8
Mahir-Neema <mohi10503@gmail.com>
2023-05-18 00:15:44 +0530

    adding multer to upload images

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 
+const multer = require("multer");
+
 dotenv.config();
 app.use(express.json());
 
@@ -16,6 +18,18 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Database connected'))
   .catch(e => console.error(e));
 
+  // const storage = multer.diskStorage({
+  //   destination:(req,file,cb)=>{
+  //     cb(null,"images")
+  //   }, filename:(req,file,cb) => {
+  //     cb(null,req.body.name);
+  //   }
+  // });
+
+  // const upload = multer({storage:storage});
+  // app.post("/api/upload",upload.single("file"),(req,res)=>{
+  //   res.status(200).json("File has been uploaded");
+  // })
 
 
 app.use("/api/auth", authRoute);
