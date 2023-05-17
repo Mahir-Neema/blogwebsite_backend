ae8668ad289f25e2e36267e1b910cf2972b8455c
Mahir-Neema <mohi10503@gmail.com>
2023-05-17 23:56:34 +0530

    category route done

--- routes/categories.js
+++ routes/categories.js
@@ -1,12 +1,15 @@
 const router = require("express").Router();
+const Category = require("../models/Category");
 const Categories = require("../models/Category");
 
-
-
-
-
-
-
-
+router.post("/", async (req,res)=>{
+    const newCat = new Category(req.body);
+    try{
+        const savedCat = await newCat.save();
+        res.status(200).json(savedCat);
+    }catch(e){
+        res.status(500).json(e);
+    }
+});
 
 module.exports = router;
