e8a7cf9570b4d39ec0f3f56aeee6369c7519de35
Mahir-Neema <mohi10503@gmail.com>
2023-05-17 19:12:27 +0530

    delete post working

--- routes/posts.js
+++ routes/posts.js
@@ -39,6 +39,24 @@ router.put("/:id", async (req, res) => {
 });
 
 //deleting a post
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        await post.deleteOne();
+        // await post.delete();
+        res.status(200).json("Post has been deleted");
+      } catch (e) {
+        res.status(500).json("error: "+e);
+      }
+    } else {
+      res.status(401).json("You can delete only your post!");
+    }
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
 
 // get post
 router.get("/:id", async (req, res) => {
