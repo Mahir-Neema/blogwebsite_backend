822486e91d37034ec6b77eeebcb03030bbc93d73
Mahir-Neema <mohi10503@gmail.com>
2023-05-17 17:12:59 +0530

    update post working

--- routes/posts.js
+++ routes/posts.js
@@ -2,53 +2,53 @@ const router = require("express").Router();
 const User = require("../models/User");
 const Post = require("../models/Post");
 
-
 //creating a new post
-router.post("/", async(req,res)=>{
-    const newPost = new Post(req.body);
-    try{
-        const savedPost = await newPost.save();
-        res.status(200).json(savedPost);
-    }catch(e){
-        res.status(500).json(e);
-    }
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (e) {
+    res.status(500).json(e);
+  }
 });
 
 //updating a post
-router.delete("/:id", async(req,res)=>{
-    if(req.body.userId == req.params.id){
-        try{
-            const user = await User.findById(req.params.id);
-            try{
-                await Post.deleteMany({username:user.username});
-                await User.findByIdAndDelete(req.params.id);
-                res.status(200).json("user has been deleted");
-            } catch(e){
-                res.status(500).json(e);
-            }
-        }catch(e){
-            res.status(404).json("User not found");
-        }
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedPost);
+      } catch (e) {
+        res.status(500).json(e);
+      }
+    } else {
+      res.status(401).json("You can update only your post!");
     }
-    else{
-        res.status(401).json("You can delete only your account");
-    }
+  } catch (e) {
+    res.status(500).json(e);
+  }
 });
-
 
 //deleting a post
 
 // get post
-router.get("/:id", async (req,res) => {
-    try{
-        const user = await User.findById(req.params.id);
-        const {password, ...others} = user._doc;
-        res.status(200).json(others);
-    }catch(e){
-        res.status(500).json(e);
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (e) {
+    res.status(500).json(e);
+  }
 });
-
-
 
 module.exports = router;
